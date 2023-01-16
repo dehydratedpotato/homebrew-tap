78e86b13a1ba879adc9da247323095088b525c9a
BitesPotatoBacks <83843298+BitesPotatoBacks@users.noreply.github.com>
2023-01-16 09:10:18 -0800

    Update for v0.3.3

--- Formula/socpwrbud.rb
+++ Formula/socpwrbud.rb
@@ -2,8 +2,8 @@ class Socpwrbud < Formula
     desc "A Sudoless Apple Silicon tool able to retrieve per-core active frequencies, residency, power, and more."
     homepage "https://github.com/BitesPotatoBacks/SocPowerBuddy"
     url "https://github.com/BitesPotatoBacks/SocPowerBuddy/releases/latest/download/socpwrbud.tar.gz"
-    sha256 "9a2a464432ca21f305c85c379f865b407414eaf409ba3f0ec7f86c94f2738423"
-    version "0.3.2"
+    sha256 "74da6b5c0f6a833100ab15296b498143555066bbc28a245a5425153e3c8d962c"
+    version "0.3.3"
 
     def install
         bin.install "socpwrbud"
