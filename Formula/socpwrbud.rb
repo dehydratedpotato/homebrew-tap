85de4b6828b654b581b011e60456df759a7bb332
dehydratedpotato <83843298+dehydratedpotato@users.noreply.github.com>
2023-05-12 07:27:01 -0700

    Update url for username change

--- Formula/socpwrbud.rb
+++ Formula/socpwrbud.rb
@@ -1,7 +1,7 @@
 class Socpwrbud < Formula
     desc "For Apple Silicon - A sudoless command-line utility to profile per-core frequencies, cycles, instructions, power, and more!"
-    homepage "https://github.com/BitesPotatoBacks/SocPowerBuddy"
-    url "https://github.com/BitesPotatoBacks/SocPowerBuddy/releases/latest/download/socpwrbud.tar.gz"
+    homepage "https://github.com/dehydratedpotato/SocPowerBuddy"
+    url "https://github.com/dehydratedpotato/SocPowerBuddy/releases/latest/download/socpwrbud.tar.gz"
     sha256 "8ba8a81e6ea852a90b8b75a99cc1b0196d33040257b4adbfb6f3d4e5c630c12b"
     version "0.4"
 
